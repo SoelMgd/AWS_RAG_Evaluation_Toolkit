7ecd2eb985e546ceb7ba56725018e8d55d3add73
MEGDOUD-SC <soel.megdoud@eu.corp.airliquide.com>
2024-09-18 12:43:43 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AWS_RAG_Toolkit',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    # Ne spécifiez pas install_requires ici
+)
